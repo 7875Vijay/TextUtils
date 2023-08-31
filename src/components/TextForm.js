b32a7f7fe414624182e8ec15f1be0372c65aacea
Vijay <vp0013621@gmail.com>
2023-08-31 10:55:05 +0530

    added the convert in to lowercase button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,28 +1,35 @@
 import React,{useState} from 'react'  //useState is the hooks used for the text area
 
 export default function TextForm(props) {
-    
     const [text, setText] = useState()
     //text = "this is the converted text"   //this is the wrong way to change the text
     //setText("this is the converted text") //this is the right way to change the text
 
     const handelUPclick = ()=>{
-        console.log(text)
         setText(text.toUpperCase())
-        console.log(text)
+    }
+    const handelLOclick = ()=>{
+        setText(text.toLowerCase())
     }
     const onchangeEvent = (event)=>{
-        console.log("onchanged fired")
         setText(event.target.value)
     }
 
     return (
         <>
+        <div className='container my-3'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" id="mybox" rows="15" onChange={onchangeEvent} value={text}></textarea>
             </div>
-            <button className='btn btn-primary' onClick={handelUPclick}>Convert to uppercase</button>
+            <button className='btn btn-primary mx-1' onClick={handelUPclick}>Convert to UpperCase</button>
+            <button className='btn btn-primary mx-1' onClick={handelLOclick}>Convert to LowerCase</button>
+        </div>
+        <div className='container my-3'>
+            <h1>Text summary</h1>
+            <p> {text.split(" ").length} words and {text.length}</p>
+            <p> {0.008*text.split(" ").length}minuts will take to read the above paragraph</p>
+        </div>
         </>
     )
 }
